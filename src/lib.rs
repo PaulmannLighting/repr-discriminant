ca3c2954222ef1c4a1c38bfc5aaf8e00ba3ae4f7
Richard Neumann <neumann@paulmann.de>
2025-07-24 10:15:46 +0200

    Convert to simplified derive macro

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,23 @@
 //! Attribute macro to implement a discriminant method for enums with a specific representation type.
 
 use proc_macro::TokenStream;
-use proc_macro2::Span;
 use quote::quote;
-use syn::parse::{Parse, ParseStream};
-use syn::token::Comma;
-use syn::{parse_macro_input, Data, DeriveInput, Ident, Type};
-
-const DEFAULT_DISCRIMINANT_METHOD_NAME: &str = "discriminant";
+use syn::{Data, DeriveInput, Ident, parse_macro_input};
 
 /// Attribute macro to implement a discriminant method for enums with a specific representation type.
 ///
 /// # Panics
 ///
-/// This macro will panic if the input type is not an enum or if the arguments are not specified correctly.
-#[proc_macro_attribute]
-pub fn repr_discriminant(args: TokenStream, input: TokenStream) -> TokenStream {
-    let args: Args = parse_macro_input!(args);
-    let typ = args.typ;
-    let method_name = args
-        .method_name
-        .unwrap_or_else(|| Ident::new(DEFAULT_DISCRIMINANT_METHOD_NAME, Span::call_site()));
-    let input: DeriveInput = parse_macro_input!(input as DeriveInput);
+/// This macro will panic if the input type is not an enum with a valid `#[repr(T)]`.
+#[proc_macro_derive(ReprDiscriminant)]
+pub fn repr_discriminant(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+    let repr_type: Ident = input
+        .attrs
+        .iter()
+        .filter(|attr| attr.path().is_ident("repr"))
+        .find_map(|attr| attr.parse_args().ok())
+        .expect("`#[repr(T)]` is required");
     let generics = &input.generics;
     let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
     let name = &input.ident;
@@ -32,12 +28,12 @@ pub fn repr_discriminant(args: TokenStream, input: TokenStream) -> TokenStream {
                 ///
                 /// # Safety
                 ///
-                /// This method is safe, because the macro guarantees that the enum is repr(#typ).
-                pub const fn #method_name(&self) -> #typ {
+                /// This method is safe, because the macro guarantees that the enum is repr(T).
+                pub const fn discriminant(&self) -> #repr_type {
                     #[allow(unsafe_code)]
                     unsafe {
                         *::core::ptr::from_ref(self)
-                            .cast::<#typ>()
+                            .cast::<#repr_type>()
                     }
                 }
             }
@@ -46,43 +42,10 @@ pub fn repr_discriminant(args: TokenStream, input: TokenStream) -> TokenStream {
     };
 
     TokenStream::from(quote! {
-        #[repr(#typ)]
         #input
 
         impl #impl_generics #name #ty_generics #where_clause {
             #body
         }
     })
-}
-
-
-/// Arguments for the `repr_discriminant` macro.
-struct Args {
-    typ: Type,
-    method_name: Option<Ident>,
-}
-
-impl Parse for Args {
-    fn parse(input: ParseStream<'_>) -> syn::Result<Self> {
-        let typ: Type = input.parse()?;
-
-        if input.is_empty() {
-            return Ok(Self {
-                typ,
-                method_name: None,
-            });
-        };
-
-        let _: Comma = input.parse()?;
-        let method_name = input.parse()?;
-
-        if !input.is_empty() {
-            return Err(input.error("Unexpected tokens after method name"));
-        }
-
-        Ok(Self {
-            typ,
-            method_name,
-        })
-    }
 }
