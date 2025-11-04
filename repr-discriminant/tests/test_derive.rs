78748ae5a03e131e7396437a6c3b85cd3d1464ac
Richard Neumann <neumann@paulmann.de>
2025-11-04 15:27:39 +0100

    Add generics test.

--- repr-discriminant/tests/test_derive.rs
+++ repr-discriminant/tests/test_derive.rs
@@ -2,6 +2,8 @@
 #![cfg(test)]
 #![cfg(feature = "derive")]
 
+use std::fmt::Debug;
+
 use repr_discriminant::ReprDiscriminant;
 
 #[repr(u8)]
@@ -31,4 +33,20 @@ fn test_trait_discriminant() {
     assert_eq!(FOO.repr_discriminant(), 1u8);
     assert_eq!(BAR.repr_discriminant(), 2u8);
     assert_eq!(SPAM.repr_discriminant(), 3u8);
+}
+
+#[test]
+#[allow(dead_code)]
+fn test_generic() {
+    assert_discriminant(FOO, 1u8);
+    assert_discriminant(BAR, 2u8);
+    assert_discriminant(SPAM, 3u8);
+}
+
+fn assert_discriminant<T, U>(value: T, expected: U)
+where
+    T: ReprDiscriminant<Repr = U>,
+    U: PartialEq + Debug,
+{
+    assert_eq!(value.repr_discriminant(), expected);
 }
